3f8a133be30f410203e106f58babec4d150ab46e
João Moreira Damacena Neto <jmdneto@hotmail.com>
2021-01-26 09:36:05 -0300

    Exclusão de arquivo.

--- RickLocalization.Tests/ViagemTests.cs
+++ RickLocalization.Tests/ViagemTests.cs
@@ -19,17 +19,21 @@ namespace RickLocalization.Tests
         }
 
         [Fact]
-        public void CriarViagem_DadosCompletos_DeveRetornarOk()
+        public void CriarViagem_DadosCompletos_DeveRetornarNaoNulo()
         {
+            //Arrange
             //Criando personagem:
             _personagemDimensao = new Dimensao(1, "C-130");
             _personagem = new Personagem(1, "Ricky", _personagemDimensao, "http://labcon.fafich.ufmg.br/wp-content/uploads/2018/06/5164749-940x429.jpg");
 
             //Criando viagem:
             _origem = new Dimensao(1, "C-130");
-            _destino = new Dimensao(2, "C-131");
+            _destino = new Dimensao(2, "C-131");            
+
+            //Act
             _viagem = new Viagem(1, _personagem, _origem, _destino);
 
+            //Assert
             Assert.NotNull(_viagem);
 
         }
